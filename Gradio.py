22221ec074c9d459058be59e41014593caa4f3ad
Haris Ansar Khan <techies.ml.ai@gmail.com>
2023-09-10 05:53:08 +0530

    gradio

--- Gradio.py
+++ Gradio.py
@@ -6,13 +6,11 @@ from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from tensorflow.keras.models import load_model
 import cv2
 
-# Load the best model
 model_path = "car_classification_model.h5"
 best_model = load_model(model_path)
 
 class_labels = ['Audi', 'Hyundai Creta', 'Mahindra Scorpio', 'Rolls Royce', 'Swift', 'Tata Safari', 'Toyota Innova']
 
-# Define a tf.function for prediction
 @tf.function
 def predict_image(image_array):
     prediction = best_model(image_array)
@@ -20,27 +18,21 @@ def predict_image(image_array):
     predicted_class = tf.gather(class_labels, class_index)
     return predicted_class
 
-# Predict function with breed name as string
 def Predict_Car_Brand(image_upload):
-    # Convert the PIL image to a NumPy array
     image_array = np.array(image_upload)
     
-    # Resize the image to (224, 224)
     image_resized = cv2.resize(image_array, (224, 224))
     
     img_array = img_to_array(image_resized)
     img_array = np.expand_dims(img_array, axis=0)
-    img_array /= 255.0  # Normalize the image
-    
-    # Convert to TensorFlow tensor
+    img_array /= 255.0 
+
     image_tensor = tf.convert_to_tensor(img_array, dtype=tf.float32)
     
-    # Predict using the tf.function
     predicted_brand = predict_image(image_tensor)
     label = predicted_brand.numpy()[0].decode()
     return label
 
-# Create and launch the Gradio interface
 demo = gr.Interface(
     Predict_Car_Brand, 
     inputs = "image",
